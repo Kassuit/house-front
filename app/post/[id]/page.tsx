64220895c209eaf020b811a19916fcef726fc375
Gabucado <awsalinas@uc.cl>
2025-01-18 12:11:10 -0300

    🏗️ Refactor: Trimmed Component Name

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { PostComponent } from "@/components/Post/Post";
+import { Post } from "@/components/Post/Post";
 import { PostAPIInterface } from "@/types/Post";
 import { UserAPIInterface } from "@/types/User";
 import ApiRequest from "@/utils/api_request";
+import host from "@/utils/host_getter";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -10,9 +11,8 @@ export default function Home() {
   const { id } = useParams<{ id: string }>();
   const postId = parseInt(id);
 
-  const api_handler = new ApiRequest(process.env.NEXT_PUBLIC_BACKEND_URL ?? 'localhost:3000');
+  const api_handler = new ApiRequest(host);
 
-  // TODO: Set post type
   const [ post, setPost ] = useState<PostAPIInterface>();
   const [ user, setUser ] = useState<UserAPIInterface>();
 
@@ -20,12 +20,11 @@ export default function Home() {
     api_handler.GetPostWithUser(postId)
       .then((post) => {
         setPost(post)
-        console.log(post);
         const { user } = post;
         setUser(user);
       });
   }, [])
 
   if (!user || !post) return <div>Loading...</div>;
-  return <PostComponent user={user} post={post} />
+  return <Post user={user} post={post} />
 }
--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -1,13 +1,15 @@
 import { FC } from "react";
-import { PostUserComponent, PostUserComponentProps } from "./PostUserComponent";
-import { PostContentComponent, PostContentComponentProps } from "./PostContentComponent";
+import { PostUser, PostUserProps } from "./PostUser";
+import { PostContent, PostContentProps } from "./PostContent";
 
-export const PostComponent: FC<(PostUserComponentProps | undefined) & (PostContentComponentProps | undefined)> = ({...props}) => {
+export const Post: FC<(PostUserProps | undefined) & (PostContentProps | undefined)> = ({...props}) => {
 
   const {user, post} = props;
 
+  if (!user || !post) return <> Loading... </>
+
   return <div className="flex flex-col">
-    <PostUserComponent user={user} />
-    <PostContentComponent post={post}/>
+    <PostUser user={user} />
+    <PostContent post={post}/>
   </div>
 }
